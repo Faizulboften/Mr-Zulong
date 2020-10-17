16f1119937e4e02e64f1ac9a0f84e3dbe9878907
Faizulboften <72049391+Faizulboften@users.noreply.github.com>
2020-10-17 14:46:08 +0800

    Update spam.sh

--- spam.sh
+++ spam.sh
@@ -1,4 +1,85 @@
+red='\e[1;31m'
+green='\e[1;32m'
+yellow='\e[1;33m'
+blue='\e[1;34m'
+magenta='\e[1;35m'
+cyan='\e[1;36m'
+white='\e[1;37m'
+#gantikodewarna
+k="\033[33;1m"
+m="\033[31;1m"
+pu="\033[39;1m"
+p="\033[36;1m"
+b="\033[34;1m"
+h="\033[32;1m"
+JAM=`date +%H`
+MENIT=`date +%M`
+DETIK=`date +%S`
+TANGGAL=`date +%d`
+BULAN=`date +%m`
+TAHUN=`date +%Y`
+mp="\033[41;1;39m"
+o="\033[0m"
 clear
+echo
+echo "         ${m}[ ${pu}TIME ZONE INDONESIA ${m}]"
+echo "${b}   ╔════════╗${b} ╔═══════🕜═╗${b} ╔═══════🕛═╗"
+echo "${b}   ║${k}JAM:"${m}[${pu}$JAM${m}]${b}║~${b}║${k}MENIT:"${m}[${pu}$MENIT${m}]${b}║${b}~║${k}DETIK:${m}["${pu}$DETIK${m}]${b}║
+echo "${b}   ╚════════╝${b} ╚══════════╝${b} ╚══════════╝"
+echo "               ${m}[ ${pu}KALENDER ${m}]"
+echo "${b}╔════════════╗${b} ╔══════════╗${b} ╔════════════╗"
+echo "${b}║${k}TANGGAL:"${m}[${pu}$TANGGAL${m}]${b}║${b}~║${k}BULAN:"${m}[${pu}$BULAN${m}]${b}║${b}~║${k}TAHUN:${m}["${pu}$TAHUN${m}]${b}║
+echo "${b}╚════════════╝${b} ╚══════════╝${b} ╚════════════╝"
+echo "${k}<════════════════════════════════════════>"
+echo
+
+
+limit=100
+sleep 1
+echo " \033[31;1m    ▇▇▇▇▇▇▇"
+echo "\033[37;1m     ▇▇▇▇▇▇▇"
+echo "\033[31;1mSELAMAT DATANG \033[37;1mDi Tools Faizul Kontol lu"
+sleep 1
+echo -n "My Name Is?:"
+read nama;
+sleep 3
+echo "Hallo salam kenal Sad \033[31;1m[$nama]"
+sleep 3
+echo "\033[32;1mJangan Lupa subscribe channel admin woke:)"
+sleep 3
+clear
+echo -n "apakah anda sudah subscribe channel admin? y/n :"
+read pilih
+if [ $pilih = "y" ];
+then
+sleep 2
+echo "Terimakasih sudah subscribe channel admin"
+sleep 2
+clear
+elif [ $pilih = "n" ];
+then
+sleep 2
+echo "SILAHKAN SUBSCRIBE CHANNEL ADMIN"
+termux-open-url "https://www.youtube.com/channel/UCSxkYGsMV5DvXz4GQJskl9Q"
+sleel 4
+echo "MAKASIH SUDAH DUKUNG  CHANNEL ADMIN^_^"
+sleep 2
+clear
+clear
+else
+echo "anda salah memasukan input!!!!"
+sleep 2
+sh tools.sh
+fi
+clear
+sleep 1
+python2 loding692
+sleep 1
+clear
+echo "\033[37;1m  <═════════════════════════════════════════>"
+echo "  <\>\033[37;1m  WELCOME TO MY TOOLS\033[31;1m|\033[37;1mTHE X-FaizulBoften   \033[31;1m<\>"
+echo "\033[37;1m  <═════════════════════════════════════════>"
+clear                     
 sleep 4
 echo $cyan " MENGGINSTALL COK "
 sleep 20
@@ -14,37 +95,39 @@ echo $ijo "
 \033[1;93m█▄█████▄█      \033[1;91m●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
 \033[1;93m█\033[1;92m▼▼▼▼▼ \033[1;92m- _ --_--\033[1;95m╔╦╗┌─┐┬─┐┬┌─   ╔═╗╔╗ 
 \033[1;93m█ \033[1;92m \033[1;92m_-_-- -_ --__\033[1;93m ║║├─┤├┬┘├┴┐───╠╣ ╠╩╗
-\033[1;93m█\033[1;92m▲▲▲▲▲\033[1;92m--  - _ --\033[1;96m═╩╝┴ ┴┴└─┴ ┴   ╚  ╚═╝ \033[1;96mtools-Dark
+\033[1;93m█\033[1;92m▲▲▲▲▲\033[1;92m--  - _ --\033[1;96m═╩╝┴ ┴┴└─┴ ┴   ╚  ╚═╝ \033[1;96mtools-Mr-Zulong
 \033[1;93m█████████      \033[1;92m«----------✧----------»
 \033[1;93m ██ ██"
 
 echo $cy"
 ═══════════════════════════════
-     {  📂 MENU TOOLS SPAM 📂   }
+     {  📂 <<•  @ROOT  •>> 📂   }
 ═══════════════════════════════        
 ═══════════════════════════════
 🔘 { AU : Faizul        }
 🔘 { Fb : Faizul        }
 🔘 { WA : 082271426251  }
 ═══════════════════════════════
-             ⤵⤵⤵⤵
+            <<• MENU TOOLS •>>
 ═══════════════════════════════════════
-[ 1 ] : 🔘 PP GUARD               🔘         }
+[1] : •>> PP GUARD             🔘   }
 ═══════════════════════════════════════
-[ 2 ] : 🔘 PHISING PROJECT V1     🔘         }
+[2] : •>> PHISING PROJECT V1   🔘   }
 ═══════════════════════════════════════
-[ 3 ] : 🔘 HACK AKUN NO LOGIN     🔘         }
+[3] : •>> HACK AKUN NO LOGIN   🔘   }
+═══════════════════════════════════════
+[4] : •>> HACK CCTV PCS        🔘   }
 ═══════════════════════════════════════
 [ 4 ] :EXIT/PROGRAM 🔴"
-      echo '\033[35;1m'
-      read -p "root@Pilih Nomor > " bro
+      echo '\033[37;1m'
+      read -p "root@localhost •>> > " bro
 ═══════════════════════════════════════
 
 if [ $bro = 1 ] || [ $bro = 1 ]
 then
 clear
 echo $cyan " MENGGINSTALL CUK "
-sleep 30
+sleep 10
 pkg install git
 apt install git -y
 git clone https://github.com/ARIYA-CYBER/NEW
@@ -56,7 +139,7 @@ if [ $bro = 2 ] || [ $bro = 2 ]
 then
 clear
 echo $cyan " MENGGINSTALL CUK "
-sleep 30
+sleep 10
 pkg install git
 pip2 install mechanize
 git clone hhttps://github.com/evait-security/weeman
@@ -68,7 +151,7 @@ if [ $bro = 3 ] || [ $bro = 3 ]
 then
 clear
 echo $cyan " MENGGINSTALL DULU "
-sleep 30
+sleep 10
 pkg install git
 git clone https://github.com/RIZKY4/pro
 cd pro
@@ -76,6 +159,17 @@ python2 pro.py
 fi
 
 if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+echo $cyan " MENGINSTALL DULU "
+sleep 10
+pkg install git
+git clone https://github.com/kancotdiq/ipcs
+cd ipcs
+python2 scan.py
+fi
+
+if [ $bro = 99 ] || [ $bro = 99 ]
 then
 echo $cyan  "SUBSCRIBER YT HTC CTR GAMING "
 sleep 20
